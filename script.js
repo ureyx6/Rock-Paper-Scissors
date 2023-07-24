2ac2ca398a35eb201af0837b07e8348f997ad172
Chris Whitley <ureyx6@gmail.com>
2023-07-24 14:21:47 -0700

    added a function to play a round

--- script.js
+++ script.js
@@ -16,4 +16,21 @@ let promptAnswer = prompt("Choose Rock, Paper, Or Scissors").toLowerCase();
 let playerChoice = promptAnswer.charAt(0).toUpperCase() + promptAnswer.slice(1);
 return playerChoice;
 }
-console.log(getPlayerChoice());
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == "Rock" && computerChoice == "Paper") {
+        return "You lose! Paper beats Rock";
+      } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+        return "You win! Paper beats Rock";
+      } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+        return "You win! Rock beats Scissors";
+      } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
+        return "You lose! Rock beats Scissors";
+      } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
+        return "You lose! Scissors beats Paper";
+      } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+        return "You win! Scissors beats Paper";
+      } else if (playerChoice == computerChoice) {
+        return "Draw";
+}
+}
+
