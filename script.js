ac676240110c8fa74c4e49f904b1f0db7ba0e498
Chris Whitley <ureyx6@gmail.com>
2023-07-25 22:02:35 -0700

    added display for round result

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var playerScore = 0
 var computerScore = 0
+let result = ""
 
 function getComputerChoice() {
     let randomizer = Math.floor(Math.random() * 3);
@@ -15,42 +16,39 @@ function getComputerChoice() {
     }
 }
 function getPlayerChoice() {
-    let promptAnswer = prompt("Choose Rock, Paper, Or Scissors").toLowerCase();
-    let playerChoice = promptAnswer.charAt(0).toUpperCase() + promptAnswer.slice(1);
-    return playerChoice;
+    //let promptAnswer = prompt("Choose Rock, Paper, Or Scissors").toLowerCase();
+    //let playerChoice = promptAnswer.charAt(0).toUpperCase() + promptAnswer.slice(1);
+    //return playerChoice;
 }
 function playRound(playerChoice, computerChoice) {
     if (playerChoice == "Rock" && computerChoice == "Paper") {
         computerScore += 1
-        return "You lose! Paper beats Rock";
+        result = "You lose! Paper beats Rock";
     } else if (playerChoice == "Paper" && computerChoice == "Rock") {
         playerScore += 1
-        return "You win! Paper beats Rock";
+        result = "You win! Paper beats Rock";
     } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
         playerScore += 1
-        return "You win! Rock beats Scissors";
+        result = "You win! Rock beats Scissors";
     } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
         computerScore += 1
-        return "You lose! Rock beats Scissors";
+        result = "You lose! Rock beats Scissors";
     } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
         computerScore += 1
-        return "You lose! Scissors beats Paper";
+        result = "You lose! Scissors beats Paper";
     } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
         playerScore += 1
-        return "You win! Scissors beats Paper";
+        result = "You win! Scissors beats Paper";
     } else if (playerChoice == computerChoice) {
-        return "Draw";
+        result = "Draw";
     }
+    document.getElementById('display').textContent = result
+    return
 }
 
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice, computerChoice));
-    }
-function scoreCounter() {
+    function scoreCounter() {
     } if (playerScore == computerScore) {
         console.log("Draw!");
     } else if (playerScore > computerScore) {
@@ -59,5 +57,34 @@ function scoreCounter() {
         console.log("You have lost :c.");
     }
 }
-game();
 
+const buttons = document.querySelectorAll('button')
+buttons.forEach(everyButton => {
+    let id = everyButton.getAttribute('id')
+    everyButton.addEventListener('click', function () {
+        if (id === 'Rock') {
+            playRound("Rock", getComputerChoice())
+        } else if (id === 'Paper') {
+            playRound("Paper", getComputerChoice())
+        } else {
+            playRound("Scissors", getComputerChoice())
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+//document.querySelectorAll('button').forEach(dog => {
+//    let id = dog.getAttribute('id');
+//    dog.addEventListener('click', function() {
+//      console.log('A button with ID ' + id + ' was clicked!') 
+//    } );
+//  });
